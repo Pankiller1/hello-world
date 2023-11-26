8f336f6b20c427b2fd05f22bdb05baa1c19e5928
Pankiller1 <1621140210@qq.com>
2023-11-26 11:03:51 +0800

    test

--- test/test_knapsack.py
+++ test/test_knapsack.py
@@ -1,12 +1,12 @@
 import unittest
-from src import knapsack
+from src import DP
 
 class TestKnapsack(unittest.TestCase):
     def test_knapsack(self):
         values = [60, 100, 120]
         weights = [10, 20, 30]
         capacity = 50
-        result = knapsack(values, weights, capacity)
+        result = DP.knapsack(values, weights, capacity)
         self.assertEqual(result, 220)
 
 if __name__ == '__main__':
